a07e2818e49f8ef2be18a976357951696dfecd3a
vishaljadhav7 <143590501+vishaljadhav7@users.noreply.github.com>
2023-09-21 14:18:19 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ export const LOGO_URL =
   "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
 
 export const MENU_API =
-  "https://instafood.onrender.com/api/menu?lat=12.9351929&lng=77.62448069999999&restaurantId=";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=25.4358011&lng=81.846311&restaurantId=";
 
   export const RES_IMG_CDN =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
