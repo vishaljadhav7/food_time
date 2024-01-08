e6182d7543f38d92a71cff78fd13f03d3ed36f14
vishaljadhav7 <143590501+vishaljadhav7@users.noreply.github.com>
2024-01-08 23:47:21 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,6 @@
 export const CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
-export const LOGO_URL =
-  "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
-
 export const MENU_API =
   "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=25.4358011&lng=81.846311&restaurantId=";
 
