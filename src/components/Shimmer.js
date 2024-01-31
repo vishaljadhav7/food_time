84de06bb9ae4702fa71b186b7cf51f0a0d7fc7db
vishaljadhav7 <143590501+vishaljadhav7@users.noreply.github.com>
2024-01-31 23:00:41 +0530

    Update Shimmer.js

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,24 +1,14 @@
 const Shimmer = () => {
   return (
-    <div className="shimmer-container">
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
+    <div className="shimmer-container flex flex-wrap sm:justify-center align-middle mt-6 ">
+      <div className="shimmer-card  animate-pulse  w-[350px] h-[250px] m-3 bg-slate-500 rounded-lg"></div>
+      <div className="shimmer-card  animate-pulse  w-[350px] h-[250px] m-3 bg-slate-500 rounded-lg"></div>
+      <div className="shimmer-card  animate-pulse  w-[350px] h-[250px] m-3 bg-slate-500 rounded-lg"></div>
+      <div className="shimmer-card  animate-pulse  w-[350px] h-[250px] m-3 bg-slate-500 rounded-lg"></div>
+      <div className="shimmer-card  animate-pulse  w-[350px] h-[250px] m-3 bg-slate-500 rounded-lg"></div>
+      <div className="shimmer-card  animate-pulse  w-[350px] h-[250px] m-3 bg-slate-500 rounded-lg"></div>
+      <div className="shimmer-card  animate-pulse  w-[350px] h-[250px] m-3 bg-slate-500 rounded-lg"></div>
+      <div className="shimmer-card  animate-pulse  w-[350px] h-[250px] m-3 bg-slate-500 rounded-lg"></div>
     </div>
   );
 };
