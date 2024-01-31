e9dd6b634b07c3c2c5f75e0b8755fb4dc8c2ee96
vishaljadhav7 <143590501+vishaljadhav7@users.noreply.github.com>
2024-01-31 22:58:35 +0530

    Updated Ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { LOGO_URL } from "../utils/constants";
+
 import foodtime from "../../assets/foodtime.png";
 import { useState, useContext } from "react";
 import { Link } from "react-router-dom";
@@ -20,29 +20,35 @@ const Header = () => {
 
   return (
     <div className="flex justify-between  shadow-lg">
-      <div className="logo-container w-24 h-24">
-        <img className="w-56" src={foodtime} />
+      <div className="logo-container w-24 h-24 p-1 mx-2 ">
+        <img className="w-56 rounded-lg  " src={foodtime} />
       </div>
       <div className="flex items-center">
-        <ul className="flex p-4 m-4">
+        <ul className="flex p-2 m-2 gap-4">
+           {/* sfsd     
+             <a href="#_" class="px-5 py-2.5 font-medium bg-blue-50 hover:bg-blue-100 hover:text-blue-600 text-blue-500 rounded-lg text-sm">
+Button Text
+</a>
+           */}
+
           <li className="px-4">Online Status: {onlineStatus ? "✅" : "🔴"}</li>
           <li className="px-4">
-            <Link to="/">Home</Link>
+            <Link to="/"><button type="button" className="p-2 font-medium bg-blue-50 hover:bg-blue-100 hover:text-blue-600 text-blue-500 rounded-lg text-sm" >Home</button></Link>
           </li>
           <li className="px-4">
-            <Link to="/about">About</Link>
+            <Link to="/about"><button type="button" className="p-2 font-medium bg-blue-50 hover:bg-blue-100 hover:text-blue-600 text-blue-500 rounded-lg text-sm" >About</button></Link>
           </li>
           <li className="px-4">
-            <Link to="/contact">Contact</Link>
+            <Link to="/contact"><button type="button" className="p-2 font-medium bg-blue-50 hover:bg-blue-100 hover:text-blue-600 text-blue-500 rounded-lg text-sm" >Contact</button></Link>
           </li>
           <li className="px-4">
-            <Link to="/grocery">Grocery</Link>
+            <Link to="/grocery"><button type="button" className="p-2 font-medium bg-blue-50 hover:bg-blue-100 hover:text-blue-600 text-blue-500 rounded-lg text-sm">Grocery</button></Link>
           </li>
-          <li className="px-4 font-bold">
+          <li className="p-2 font-medium bg-blue-50 hover:bg-blue-100 hover:text-blue-600 text-blue-500 rounded-lg text-sm">
             <Link to="/cart">Cart {cartItems.length}</Link>
           </li>
           <button
-            className="login"
+            className="p-2 font-medium bg-blue-50 hover:bg-blue-100 hover:text-blue-600 text-blue-500 rounded-lg text-sm"
             onClick={() => {
               btnNameReact === "Login"
                 ? setBtnNameReact("Logout")
