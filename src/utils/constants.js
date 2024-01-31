0a26eecd34e6a013b5799b40d07ea8a3ea69deaf
vishaljadhav7 <143590501+vishaljadhav7@users.noreply.github.com>
2024-01-31 22:59:26 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,21 @@ export const CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const MENU_API =
-  "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=25.4358011&lng=81.846311&restaurantId=";
+"https://corsproxy.org/?" +
+encodeURIComponent(
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=25.4358011&lng=81.846311&restaurantId="
+);
+
+
+  
 
   export const RES_IMG_CDN =
+
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+
+  export const API_URL = 
+  "https://corsproxy.org/?" +
+  encodeURIComponent(
+    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.51981990724166&lng=73.86026275822753&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+  );
