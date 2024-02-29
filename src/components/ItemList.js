6a6a22cb80a829dc59252f2c01dfa6cf787249e8
vishaljadhav7 <143590501+vishaljadhav7@users.noreply.github.com>
2024-02-29 19:45:12 +0530

    Update ItemList.js

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -39,7 +39,7 @@ const ItemList = ({ items, dummy }) => {
                 onClick={() => handleAddItem(item)}
               >
   
-              +
+               Add+
               </button>
             </div>
           </div>
