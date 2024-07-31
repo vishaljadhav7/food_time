592962570a8992ab087db85608b6926be90189e0
Vishal Jadhav <143590501+vishaljadhav7@users.noreply.github.com>
2024-07-31 20:50:44 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,13 @@
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w660/";
 
-export const MENU_API =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=25.4358011&lng=81.846311&restaurantId=";
+export const MENU_API = `https://api.allorigins.win/get?url=${encodeURIComponent(
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.51981990724166&lng=73.86026275822753&restaurantId=",
+)}`;
 
   export const RES_IMG_CDN =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
-  export const API_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.4358011&lng=81.846311&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+  export const API_URL = `https://api.allorigins.win/get?url=${encodeURIComponent(
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.51981990724166&lng=73.86026275822753&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
+)}`;
